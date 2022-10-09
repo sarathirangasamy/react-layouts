ee6088849a0eb2fa0aefc590dd5e8796edd87a60
sarathirangasamy <sarathirangasamy@gmail.com>
2022-10-09 13:11:53 +0530

    change text and add css

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,11 @@ function App() {
               <h3>Column 2</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
               <p>
+                Get suggestions from Grammarly while you write in desktop
+                applications and sites across the web—as you move between apps,
+                social media, documents, messages, and emails.
+              </p>
+              <p>
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco
                 laboris...
               </p>
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -7,6 +7,7 @@ function Index() {
                 <img src={logo} className="App-logo" alt="logo" />
                 <div className="container">
                     <div className="row">
+                        <span>WElcome</span>
                         <master></master>
                     </div>
                 </div>
